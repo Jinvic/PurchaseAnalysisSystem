dc02369a982248784b509900818b470a1faaf0b1
Jinvic <2528976435@qq.com>
2024-04-11 19:17:17 +0800

    中期状态

--- Spider.py
+++ Spider.py
@@ -1,6 +1,14 @@
 import requests
 from pyquery import PyQuery as pq
-from selenium_class import Selenium
+# from selenium_class import Selenium
+import selenium_login
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import selenium.common.exceptions as SE
+from selenium.webdriver.common.keys import Keys
+import time
 
 # index_url_jd = 'https://www.jd.com'
 # session, _ = selenium_login.selenium()
@@ -19,6 +27,7 @@ def get_goods_id_jd(keywords):
     # selenium_jd = Selenium(login_url_jd, index_url_jd, search_url_jd)
     # selenium_jd.selenium_login()
     # selenium_jd.selenium_search(keywords)
+    selenium_login.selenium_search(keywords)
 
     # 解析网页数据
     with open('search_result.html', 'r', encoding='utf-8') as f:
@@ -30,9 +39,83 @@ def get_goods_id_jd(keywords):
         id_list.append(goods.attr('data-sku'))
         # print(goods.attr('data-sku'))
 
-    for id in id_list:
-        print(id)
+    # for id in id_list:
+    #     print(id)
+
+    return id_list
+
+
+def get_history_price(goods_id):
+    goods_url = 'https://item.jd.com/'+goods_id+'.html'
+    print(goods_id)
+    print(goods_url)
+    # target_url = 'https://www.gwdang.com/v2/trend/'+goods_id+'-'+platform_code+'.html'
+    # user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+    # headers = {'User-Agent': user_agent}
+    # response = requests.get(search_url, headers=headers)
+    # print(response.status_code)
+
+    browser = webdriver.Chrome()
+    browser.maximize_window()
+    browser.get('https://www.gwdang.com/v2/trend?from=search')
+    # 绕过webdriver验证
+    script = 'Object.defineProperty(navigator, "webdriver", { get: () => false, });'
+    browser.execute_script(script=script)
+
+    # 等待页面加载完成
+    WebDriverWait(browser, 10).until(
+        EC.presence_of_element_located((By.ID, 'url')))
+    input = browser.find_element(By.CSS_SELECTOR, '#url')
+    button = browser.find_element(By.CSS_SELECTOR, '#search-button')
+    input.send_keys(goods_url)
+    input.send_keys(Keys.ENTER)
+    time.sleep(0.2)
+    # 设定动作链
+    action = webdriver.ActionChains(browser)
+    # 跳过第一次
+    action.send_keys(Keys.TAB).perform()
+    time.sleep(0.2)
+    action.send_keys(Keys.TAB).perform()
+    time.sleep(0.2)
+    # 循环一次
+    while (button != browser.switch_to.active_element):
+        action.send_keys(Keys.TAB).perform()
+        time.sleep(0.2)
+    action.send_keys(Keys.ENTER).perform()
+
+    # 等待页面加载完成
+    WebDriverWait(browser, 15).until(
+        EC.presence_of_element_located((By.CLASS_NAME, 'promotion-item')))
+
+    # with open("price_result.html", mode="w", encoding='utf-8', newline='') as f:
+    #     f.write(browser.page_source)
+    # with open('price_result.html', 'r', encoding='utf-8') as f:
+    #     html = f.read()
+    # doc = pq(html)
+
+    doc = pq(browser.page_source)
+    datalist = doc('.promotion-list .promotion-item')
+    datalist = datalist.items()
+    price_list = list()
+    for data in datalist:
+        d = dict()
+        d['date'] = data.find('.date').text()  # 日期
+        d['price'] = data.find('.ymj span').text()[1:]  # 价格
+        price_list.append(d)
+        # print(d['date']+'\t'+d['price'])
+
+    print(len(price_list))
+    with open(goods_id+'.txt', mode='w', encoding='utf-8', newline='') as f:
+        for li in price_list:
+            f.write(li['date']+'\t'+li['price']+'\n')
+
+    return price_list
 
 
 if __name__ == '__main__':
-    get_goods_id_jd(['北通', '手柄'])
+    id_list = get_goods_id_jd(['北通', '手柄'])
+    for id in id_list:
+        print(id)
+        get_history_price(id)
+        break
+    # get_history_price('https://item.jd.com/4979408.html')
--- selenium_login.py
+++ selenium_login.py
@@ -69,22 +69,23 @@ def slide_captcha(browser, offset):
     sum_x = 0
     sum_y = 0
     while (sum_x < offset):
-        rand_y = random.randint(-5, 5)  # y轴随机抖动
+        rand_y = random.randint(-3, 3)  # y轴随机抖动
         while (sum_y+rand_y >= 30):  # 控制y轴移动不超过滑块范围
-            rand_y = random.randint(-5, 5)
+            rand_y = random.randint(-3, 3)
         rand_x = random.randint(1, 5)  # x轴随机移动
         if (offset-sum_x < 5):
             rand_x = offset-sum_x
-        action.move_by_offset(rand_x, rand_y).perform()
+        action.move_by_offset(rand_x, rand_y)
         sum_x = sum_x+rand_x
         sum_y = sum_y+rand_y
         # print(rand_x, rand_y, sum_x, sum_y)
     action.release().perform()  # 松开
 
 
-def selenium():
+def selenium_login():
     browser = webdriver.Chrome()
-    wait = WebDriverWait(browser, 5)
+    # browser.maximize_window()
+    wait = WebDriverWait(browser, 3)
     target_url = "https://passport.jd.com/new/login.aspx"
     browser.get(url=target_url)
 
@@ -110,13 +111,13 @@ def selenium():
         offset = None
         while (offset == None):
             refresh_captcha(browser)  # 刷新验证码
-            time.sleep(1)
+            time.sleep(0.5)
             offset = get_offset(browser)  # 获取滑动距离
         slide_captcha(browser, offset)
 
         try:
             wait.until(EC.text_to_be_present_in_element(
-                (By.CLASS_NAME, 'JDJRV-slide-bar-center'),'拼接成功'))
+                (By.CLASS_NAME, 'JDJRV-slide-bar-center'), '拼接成功'))
         except SE.TimeoutException:
             break
 
@@ -124,13 +125,34 @@ def selenium():
     # print(cookies)
     browser.close()
 
-    session = requests.Session()
-    for cookie in cookies:
-        session.cookies.set(cookie['name'], cookie['value'])
-    print(session)
+    # session = requests.Session()
+    # for cookie in cookies:
+    #     session.cookies.set(cookie['name'], cookie['value'])
+    # print(session)
 
-    return session, cookies
+    return cookies
+
+
+# 通过关键字搜索
+def selenium_search(keywords):
+    index_url = 'https://www.jd.com/'
+    search_url = 'https://search.jd.com/Search?keyword='
+    for keyword in keywords:
+        search_url = search_url+keyword+'%20'  # 空格分割关键字
+
+    cookies = selenium_login()
+    browser = webdriver.Chrome()
+    browser.maximize_window()
+    browser.get(index_url)
+    for cookie in cookies:
+        browser.add_cookie(cookie)
+    browser.get(search_url)
+    time.sleep(5)
+    print(browser.page_source)
+    with open("search_result.html", mode="w", encoding='utf-8', newline='') as f:
+        f.write(browser.page_source)
+    browser.close()
 
 
 if __name__ == '__main__':
-    selenium()
+    selenium_search(['北通', '手柄'])
